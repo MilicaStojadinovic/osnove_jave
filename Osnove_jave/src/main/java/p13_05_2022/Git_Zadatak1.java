ff7cca34e0202e01af882eb1b9d522edfdda2bbf
Milica Stojadinovic <mstojadinovic995@gmail.com>
2022-05-13 20:39:57 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ Osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,13 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("Prva git naredba: git select.");
+        System.out.println("Prva git naredba: clear.");
+        System.out.println("Prva git naredba: git add.");
+        System.out.println("Prva git naredba: git commit.");
+        System.out.println("Prva git naredba: git push.");
+        System.out.println("Prva git naredba: git restore.");
+
+    }
+}
