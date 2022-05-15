40c39c16d692c48f0b5e57db5bf50cff299de7b7
Milica Stojadinovic <mstojadinovic995@gmail.com>
2022-05-15 16:50:23 +0200

    Domaci 13.05.2022 Zadatak 4 - Update

--- Osnove_jave/src/main/java/d13_05_2022/Zadatak4.java
+++ Osnove_jave/src/main/java/d13_05_2022/Zadatak4.java
@@ -1,2 +1,39 @@
-package d13_05_2022;public class Zadatak4 {
+package d13_05_2022;
+
+import java.util.Scanner;
+
+public class Zadatak4 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+//Napisati program koji uci korisnika da kaze “Molim te”.
+// Program od korisnika ucitava zahteve koje treba da izvrsi ali jedino reaguje ukoliko korisnik lepo zamoli
+// ( u zahtevu unese “Molim te”). Ukoliko korisnik unese zahtev u vidu naredjivanja,
+// program ne reaguje na takve zahteve tj. nista ne ispisuje, a ukoliko korisnik zamoli tada program ispisuje
+// “Dogovoreno!”. Program radi dok korisnik ne unese “Hvala!”. Na kraju programa ispisati “Cao”
+//Commitujte zadatak i push-ujte na github, sa porukom “Domaci 13.05.2022 Zadatak 4”.
+// Za ovaj zadatak trebace vam nextLine metoda sa scannerom.
+        //Primer izvrsenja:
+        //Unesite zahtev: Molim te, podseti me sutra da kupim sapun.
+        //Dogovoreno!
+        //Unesite zahtev: Podseti me sutra i za sampon.
+        //Unesite zahtev: Molim te, podseti me da kupim prasak!
+        //Dogovoreno!
+        //Unesite zahtev: Hvala!
+        //Cao
+
+        String zahtev = "";
+        while (!zahtev.equals("Hvala!")) {
+            System.out.print("Unestite zahtev: ");
+            zahtev = s.nextLine();
+            if (zahtev.contains("Molim te")) {
+                System.out.println("Dogovoreno!");
+            } else {
+                System.out.println();
+            }
+        }
+        System.out.println("Cao!");
+    }
 }
+
+
